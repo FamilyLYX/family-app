5b80ef26e5b089497dd68dadddd964fb04ab5f4b
mumeen <azeezmumeen6@gmail.com>
2023-11-11 07:49:12 +0100

    import ABIs to the app

--- src/hooks/useCollection.ts
+++ src/hooks/useCollection.ts
@@ -1,26 +1,32 @@
-import { abi } from "family-contracts/artifacts/contracts/IdentifiablePhygitalAsset.sol/IdentifiablePhygitalAsset.json";
+import { abi } from "../artifacts/contracts/IdentifiablePhygitalAsset.sol/IdentifiablePhygitalAsset.json";
 import { useContract } from "./useContract";
 import { TokenId } from "../common/objects";
 
-export function usePhygitalCollection () {
+export function usePhygitalCollection() {
   const phygital = useContract(import.meta.env.VITE_ASSET_CONTRACT, abi);
 
-  async function getTokens (address: string) {
-    if (!address) { return []; }
-  
-    const tokens = await phygital.getFunction('tokenIdsOf')(address);
-  
-    return Array.from(tokens).map((idStr) => TokenId.parseTokenId(idStr as string));
+  async function getTokens(address: string) {
+    if (!address) {
+      return [];
+    }
+
+    const tokens = await phygital.getFunction("tokenIdsOf")(address);
+
+    return Array.from(tokens).map((idStr) =>
+      TokenId.parseTokenId(idStr as string)
+    );
   }
 
-  async function getTokenMetadata (tokenId: TokenId) {
-    const owner = await phygital.getFunction('tokenOwnerOf')(tokenId.toString());
+  async function getTokenMetadata(tokenId: TokenId) {
+    const owner = await phygital.getFunction("tokenOwnerOf")(
+      tokenId.toString()
+    );
 
     return {
       name: "Honft",
       description: "Black Forest",
-      owner: owner
-    }
+      owner: owner,
+    };
   }
 
   return { phygital, getTokens, getTokenMetadata };
