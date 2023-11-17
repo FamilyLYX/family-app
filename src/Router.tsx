3ef84b34e8ddfe89b0d9d21350f92bdc0a5b0c83
uday-kiran77 <udaykiranbujunuri@gmail.com>
2023-11-17 08:19:58 +0530

    completed product detail page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,10 +2,10 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
+  RouterProvider,
 } from "react-router-dom";
 
-import NiceModal from '@ebay/nice-modal-react';
+import NiceModal from "@ebay/nice-modal-react";
 // import { QueryClient, useQueryClient } from "@tanstack/react-query";
 
 import Layout from "./pages/Layout";
@@ -19,39 +19,45 @@ import Orders from "./pages/inventory/orders";
 import Admin from "./pages/admin/Admin";
 import RegisterToken from "./pages/register";
 
-
-import BuyModal from './common/BuyModal';
+import BuyModal from "./common/BuyModal";
 import RegisterModal from "./common/RegisterModal";
 import { Order } from "./pages/order";
 import { Login } from "./pages/login";
 import ListOnMarketplaceModal from "./common/ListOnMarketplaceModal";
+import ProductCard from "./pages/ProductCard/ProductCard";
 
-NiceModal.register('family-buy-modal', BuyModal);
-NiceModal.register('family-register-modal', RegisterModal);
-NiceModal.register('family-marketplace-list', ListOnMarketplaceModal);
+NiceModal.register("family-buy-modal", BuyModal);
+NiceModal.register("family-register-modal", RegisterModal);
+NiceModal.register("family-marketplace-list", ListOnMarketplaceModal);
 
-const router = () => createBrowserRouter(
-  createRoutesFromElements(
-    <>
-    <Route path='/t/:uid' element={<RegisterToken />} />
-    <Route path="/" element={<Layout />}>
-      <Route path="/" element={<Store />} />
-      <Route path='/login' element={<Login/>} />
-      <Route path="/orders/:id" element={<Order />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/trade" element={<Trade />} />
-      <Route path="/marketplace" element={<Marketplace/>}/>
-      <Route path="/admin-xyz" element={<Admin/>} />
-      <Route path="/inventory" element={<Inventory/>}>
-        <Route index={true} path='/inventory/phygitals' element={<Phygitals />} />
-        <Route path='/inventory/digitals' element={<Digitals/>}/>
-        <Route path='/inventory/orders' element={<Orders />}/>
-      </Route>
-    </Route>
-    </>
-  )
-);
+const router = () =>
+  createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/t/:uid" element={<RegisterToken />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Store />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/orders/:id" element={<Order />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/trade" element={<Trade />} />
+          <Route path="/marketplace" element={<Marketplace />} />
+          <Route path="/product-card" element={<ProductCard />} />
+          <Route path="/admin-xyz" element={<Admin />} />
+          <Route path="/inventory" element={<Inventory />}>
+            <Route
+              index={true}
+              path="/inventory/phygitals"
+              element={<Phygitals />}
+            />
+            <Route path="/inventory/digitals" element={<Digitals />} />
+            <Route path="/inventory/orders" element={<Orders />} />
+          </Route>
+        </Route>
+      </>
+    )
+  );
 
-export default function Router () {
-  return <RouterProvider router={router()} />
+export default function Router() {
+  return <RouterProvider router={router()} />;
 }
