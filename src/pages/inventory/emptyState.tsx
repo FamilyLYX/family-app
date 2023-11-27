4b3320c76f941b0522a3a3d065dd2ae7c7d9b834
uday-kiran77 <udaykiranbujunuri@gmail.com>
2023-11-27 09:15:19 +0530

    fixed ui issues in inventory and store

--- src/pages/inventory/emptyState.tsx
+++ src/pages/inventory/emptyState.tsx
@@ -1,11 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../common/buttons";
 
-export default function EmptyState ({ message }: { message?: string }) {
+export default function EmptyState({
+  message,
+  isLoading = false,
+}: {
+  message?: string;
+  isLoading?: Boolean;
+}) {
   const navigate = useNavigate();
 
-  return <div className="flex flex-col text-center align-middle bg-gray-100 py-24 rounded-xl">
-    <p className="text-xl mb-4">{ message || 'Your inventory is empty' }</p>
-    <div className="w-52 mx-auto mt-4"><Button onClick={() => navigate('/store')} variant="dark">In Store</Button></div>
-  </div>
+  return (
+    <div className="flex flex-col text-center align-middle bg-gray-100 py-24 rounded-xl">
+      <p className="text-xl mb-4">{message || "Your inventory is empty"}</p>
+      {!isLoading && (
+        <div className="w-52 mx-auto mt-4">
+          <Button onClick={() => navigate("/store")} variant="dark">
+            In Store
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 }
